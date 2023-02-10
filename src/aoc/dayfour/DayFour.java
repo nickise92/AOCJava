a12748d3fdb0b05f361c4bd2acfbc2f34cda9c66
Nick Iselle <nick.iselle@outlook.com>
2023-02-10 15:24:32 +0100

    DayFour part 1 solutions added

---
+++ src/aoc/dayfour/DayFour.java
@@ -0,0 +1,61 @@
+package aoc.dayfour;
+
+import aoc.inputfile.InputReader;
+import java.util.Scanner;
+
+public class DayFour {
+
+    public static void main(String[] args) {
+        InputReader reader = new InputReader("inputday4.txt");
+        System.out.printf("Part one result: %d%n", partOne(reader.getFileContent()));
+        System.out.printf("Part two result: %d%n", partTwo(reader.getFileContent()));
+    }
+
+    /* --- Other methods --- */
+
+    /**
+     * Return true if range A is inside range B. For instance range 2-4 is inside range 1-5.
+     * @param rangeA a String that contains the range written in the format 'min_value'-'max_value'
+     *               (I.E. 2-5).
+     * @param rangeB a String that contains the range written in the format 'min_value'-'max_value'
+     *               (I.E. 31-675).
+     * @return True, if range A is in range B, False otherwise.
+     */
+    private static boolean isInRange(String rangeA, String rangeB) {
+        int minA = Integer.parseInt(rangeA.split("-")[0]);
+        int maxA = Integer.parseInt(rangeA.split("-")[1]);
+        int minB = Integer.parseInt(rangeB.split("-")[0]);
+        int maxB = Integer.parseInt(rangeB.split("-")[1]);
+
+        return minB <= minA && maxA <= maxB;
+    }
+
+    /* --- Part One --- */
+    public static int partOne(String inputStr) {
+        Scanner scanner = new Scanner(inputStr);
+        int counter = 0;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] ranges = line.split(",");
+
+
+
+           if (isInRange(ranges[0], ranges[1])) {           // Check if LeftRange is contained in RightRange
+               counter = counter + 1;
+           } else if (isInRange(ranges[1], ranges[0])) {    // Check if RightRange is contained in LeftRange
+                counter = counter + 1;
+            }
+        }
+
+        scanner.close();
+        return counter;
+    }
+
+    /* --- Part One --- */
+    public static int partTwo(String inputStr) {
+
+
+        return 0;
+    }
+}

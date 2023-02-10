c7cac34b769a7be7b5708e052688d170a1fc7b36
Nick Iselle <nick.iselle@outlook.com>
2023-02-10 15:40:38 +0100

    DayFour part 1 solutions added

--- src/aoc/dayfour/DayFour.java
+++ src/aoc/dayfour/DayFour.java
@@ -12,7 +12,6 @@ public class DayFour {
     }
 
     /* --- Other methods --- */
-
     /**
      * Return true if range A is inside range B. For instance range 2-4 is inside range 1-5.
      * @param rangeA a String that contains the range written in the format 'min_value'-'max_value'
@@ -21,7 +20,7 @@ public class DayFour {
      *               (I.E. 31-675).
      * @return True, if range A is in range B, False otherwise.
      */
-    private static boolean isInRange(String rangeA, String rangeB) {
+    private static boolean fullyContained(String rangeA, String rangeB) {
         int minA = Integer.parseInt(rangeA.split("-")[0]);
         int maxA = Integer.parseInt(rangeA.split("-")[1]);
         int minB = Integer.parseInt(rangeB.split("-")[0]);
@@ -29,6 +28,16 @@ public class DayFour {
 
         return minB <= minA && maxA <= maxB;
     }
+
+    private static boolean overlaps(String rangeA, String rangeB) {
+        int minA = Integer.parseInt(rangeA.split("-")[0]);
+        int maxA = Integer.parseInt(rangeA.split("-")[1]);
+        int minB = Integer.parseInt(rangeB.split("-")[0]);
+        int maxB = Integer.parseInt(rangeB.split("-")[1]);
+
+        return minB <= minA && minA <= maxB || minB <=maxA && maxA <= maxB;
+    }
+
 
     /* --- Part One --- */
     public static int partOne(String inputStr) {
@@ -41,9 +50,9 @@ public class DayFour {
 
 
 
-           if (isInRange(ranges[0], ranges[1])) {           // Check if LeftRange is contained in RightRange
+           if (fullyContained(ranges[0], ranges[1])) {           // Check if LeftRange is contained in RightRange
                counter = counter + 1;
-           } else if (isInRange(ranges[1], ranges[0])) {    // Check if RightRange is contained in LeftRange
+           } else if (fullyContained(ranges[1], ranges[0])) {    // Check if RightRange is contained in LeftRange
                 counter = counter + 1;
             }
         }
@@ -54,8 +63,20 @@ public class DayFour {
 
     /* --- Part One --- */
     public static int partTwo(String inputStr) {
+        Scanner scanner = new Scanner(inputStr);
+        int counter = 0;
 
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] ranges = line.split(",");
 
-        return 0;
+            if (overlaps(ranges[0], ranges[1])) {
+                counter = counter + 1;
+            } else if (overlaps(ranges[1], ranges[0])) {
+                counter = counter + 1;
+            }
+        }
+
+        return counter;
     }
 }
